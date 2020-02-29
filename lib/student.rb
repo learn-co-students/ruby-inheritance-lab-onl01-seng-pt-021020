ba9a4b740c6c4a724cbd861479d2f14372ae8847
Steven Riggs <sfriggs21@gmail.com>
2020-02-28 19:31:40 -0700

    final solution

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,17 @@
-class Student 
+require_relative './user.rb'
+
+
+class Student < User
+  attr_accessor :knowledge
+
+
+  # instance methods
+  def initialize
+    @knowledge = []
+  end
+
+  def learn(new_knowledge)
+    @knowledge << new_knowledge
+  end
 
 end
