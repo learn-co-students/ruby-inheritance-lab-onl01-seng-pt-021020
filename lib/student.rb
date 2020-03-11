cd618deb9e34a81b0c95e8abe21c67eccbf3eebd
Amber Kerawala <amber_kerawala@berkeley.edu>
2020-03-11 06:54:57 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,15 @@
-class Student 
-
+require 'pry' 
+class Student < User 
+  
+  attr_accessor :knowledge
+  
+  def initialize(knowledge = [])
+    @knowledge = knowledge
+  end 
+  
+  def learn(string_of_knowledge) #takes in an argument of a string of knowledge and adds it to the student's knowledge array 
+    @knowledge << string_of_knowledge #returns the student's knowledge array 
+  end 
+    
+  
 end
