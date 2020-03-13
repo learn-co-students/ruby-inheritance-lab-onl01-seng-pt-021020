f2873a467afbb8eb336e63c31a32205b9ad1b3b1
Aniss Marrouch <aniss.marrouch@gmail.com>
2020-03-13 00:34:53 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,25 @@
-class Student 
+# Student
+#   inherits from the User class (FAILED - 1)
+#   #first_name
+#     has a first name (FAILED - 2)
+#   #last_name
+#     has a last name (FAILED - 3)
+#   #new
+#     initializes with an empty knowledge array (FAILED - 4)
+#   #learn
+#     takes in an argument of a string of knowledge and adds it to the student's knowledge array (FAILED - 5)
+#   #knowledge
+#     returns that student's knowledge array (FAILED - 6)
+
+class Student < User
+attr_accessor :first_name, :last_name, :knowledge
+def initialize
+   @knowledge = []
+end
+
+def learn(knowledge)
+  @knowledge << knowledge
+end
+
 
 end
---
+++ lib/user.rb
@@ -0,0 +1,11 @@
+# User
+#   has a first name (FAILED - 11)
+#   has a last name (FAILED - 12
+
+class User 
+  attr_accessor :first_name, :last_name
+  def initialize
+    @first_name
+    @last_name
+  end
+end
